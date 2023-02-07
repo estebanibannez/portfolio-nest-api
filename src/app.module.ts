e4b756809ec26ac062f812a6ebb2505094ebba98
Esteban Ibañez <esteban.ibanez@tested.cl>
2023-02-07 02:02:17 -0300

    feat: config environment connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,28 +9,19 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
     CertificatesModule,
     JobsModule,
     CategoryModule,
     CommonModule,
+
     MongooseModule.forRoot(
-      `mongodb+srv://ibanez:bRVJJTIe49wN1hGj@portfoliodb.d8pfr8w.mongodb.net/portfoliodb`,
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@portfoliodb.d8pfr8w.mongodb.net/${process.env.DATABASE_NAME}`,
     ),
-    ConfigModule.forRoot({
-      envFilePath: '.development.env',
-    }),
   ],
 })
 export class AppModule {
-  constructor() {
-    console.log(
-      process.env.DATABASE_USER,
-      process.env.DATABASE_PASSWORD,
-      process.env.DATABASE_NAME,
-    );
-
-    console.log(
-      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@portfoliodb.d8pfr8w.mongodb.net/${process.env.DATABASE_NAME}`,
-    );
-  }
+  constructor() {}
 }
